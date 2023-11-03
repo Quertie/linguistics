c4eda24f39327da569a75a4c2d1b89a021a5b7c6
Thomas Drake <thomas.drake@live.fr>
2023-11-03 23:59:11 +0100

    publish first version without formatting or audio content

--- app/LongShortDoubleO/page.tsx
+++ app/LongShortDoubleO/page.tsx
@@ -1,9 +1,75 @@
+import { Example } from "../components/Example"
+
 export default function LongShortDoubleO()
 {
     return (
         <main>
             <h1>Cook vs Kooke ([ʊ]/[u:])</h1>
-            <p>The two vowels covered in this lesson are often pronunciations of the written </p>
+            <h2>Lesson summary</h2>
+            <p>The two vowels covered in this lesson are often replaced by French speakers by a short [u] sound (the French ⟨ou⟩, as in <i>coucou</i>), which matches none of the correct pronunciations.</p>
+            <p>Unfortunately you cannot always know when to use which, as English is not good at following rules when it comes to pronunciation.</p>
+            <p>Let's begin with presenting both sounds:</p>
+            <ul>
+                <li>The [ʊ] sound<br/>
+                    It is <strong>short</strong>, and its realization depends on the speaker's accent.<br/>
+                    Speakers of RP English use the authentic [ʊ] sound, which is somewhere in between a French [ø] (⟨eu⟩, as in <i>jeu</i>) and [o] (as in <i>beau</i>).<br />
+                    Speakers of SSB English use a more relaxed sound, [ɵ]/[ʏ], which is very close to the French [ø] (<i>jeu</i>), but slightly less rounded (same as in <i>jeu</i> but lips do not form an "o")
+                </li>
+                <li>The [u:] sound<br />
+                    It is <strong>long</strong>, and its realization depends on the speaker's accent too.<br />
+                    Speakers of RP English use the authentic [u:] sound, which is very close to the French [u] (<i>coucou</i>), but held longer.<br />
+                    Speakers of SSB English use a sound often transcribed as [ᵾw], that could be approximated with an [øy] (<i>jeu</i> / <i>rue</i>).
+                </li>
+            </ul>
+            <h2>Examples</h2>
+                <ul>
+                    <li>The [ʊ] sound
+                        <ul>
+                            <li><Example Text='Understood' /></li>
+                            <li><Example Text='Could' /></li>
+                            <li><Example Text='Would' /></li>
+                            <li><Example Text='Should' /></li>
+                            <li><Example Text='Hood' /></li>
+                            <li><Example Text='Look' /></li>
+                            <li><Example Text='Hook' /></li>
+                            <li><Example Text='Book' /></li>
+                            <li><Example Text='Cook' /></li>
+                            <li><Example Text='Nook' /></li>
+                            <li><Example Text='Rook' /></li>
+                            <li><Example Text='Took' /></li>
+                            <li><Example Text='Foot' /></li>
+                            <li><Example Text='Footers' /></li>
+                        </ul>
+                    </li>
+                    <li>The [u:] sound
+                        <ul>
+                            <li><Example Text='Who' /></li>
+                            <li><Example Text='Mood' /></li>
+                            <li><Example Text='Blue' /></li>
+                            <li><Example Text='Shoe' /></li>
+                            <li><Example Text='Goose' /></li>
+                            <li><Example Text='Menu' /></li>
+                            <li><Example Text='Moon' /></li>
+                            <li><Example Text='Food' /></li>
+                            <li><Example Text='Move' /></li>
+                            <li><Example Text='True' /></li>
+                        </ul>
+                    </li>
+                    <li>Example sentences
+                        <ul>
+                            <li><Example Text='You should move the footers.' /></li>
+                            <li><Example Text='Who understood?' /></li>
+                            <li><Example Text='I should cook.' /></li>
+                            <li><Example Text='You could look at the menu.' /></li>
+                            <li><Example Text='Blue boots' /></li>
+                            <li><Example Text='Would the moon look blue?' /></li>
+                        </ul>
+                    </li>
+                </ul>
+            <h2>References</h2>
+            <ul>
+                <li><a href='https://thesoundofenglish.org/oo-pronunciation-rules/'>https://thesoundofenglish.org/oo-pronunciation-rules/</a></li>
+            </ul>
         </main>
     )
 }
---
+++ app/components/Example.tsx
@@ -0,0 +1,6 @@
+export function Example({Text}:{Text:string})
+{
+    return (
+        <div className='mt-1 rounded bg-slate-200'>{Text}</div>
+    )
+}
