374a9a746aeb81e3fdd8f9f5262352f7d79b9ba9
Thomas Drake <thomas.drake@live.fr>
2023-11-04 00:05:41 +0100

    fix

--- app/LongShortDoubleO/page.tsx
+++ app/LongShortDoubleO/page.tsx
@@ -8,15 +8,15 @@ export default function LongShortDoubleO()
             <h2>Lesson summary</h2>
             <p>The two vowels covered in this lesson are often replaced by French speakers by a short [u] sound (the French ⟨ou⟩, as in <i>coucou</i>), which matches none of the correct pronunciations.</p>
             <p>Unfortunately you cannot always know when to use which, as English is not good at following rules when it comes to pronunciation.</p>
-            <p>Let's begin with presenting both sounds:</p>
+            <p>Let&apos;s begin with presenting both sounds:</p>
             <ul>
                 <li>The [ʊ] sound<br/>
-                    It is <strong>short</strong>, and its realization depends on the speaker's accent.<br/>
+                    It is <strong>short</strong>, and its realization depends on the speaker&apos;s accent.<br/>
                     Speakers of RP English use the authentic [ʊ] sound, which is somewhere in between a French [ø] (⟨eu⟩, as in <i>jeu</i>) and [o] (as in <i>beau</i>).<br />
-                    Speakers of SSB English use a more relaxed sound, [ɵ]/[ʏ], which is very close to the French [ø] (<i>jeu</i>), but slightly less rounded (same as in <i>jeu</i> but lips do not form an "o")
+                    Speakers of SSB English use a more relaxed sound, [ɵ]/[ʏ], which is very close to the French [ø] (<i>jeu</i>), but slightly less rounded (same as in <i>jeu</i> but lips do not form an &quot;o&quot;)
                 </li>
                 <li>The [u:] sound<br />
-                    It is <strong>long</strong>, and its realization depends on the speaker's accent too.<br />
+                    It is <strong>long</strong>, and its realization depends on the speaker&apos;s accent too.<br />
                     Speakers of RP English use the authentic [u:] sound, which is very close to the French [u] (<i>coucou</i>), but held longer.<br />
                     Speakers of SSB English use a sound often transcribed as [ᵾw], that could be approximated with an [øy] (<i>jeu</i> / <i>rue</i>).
                 </li>
