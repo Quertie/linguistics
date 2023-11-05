d077cdc1a76b7911f2653de159d511d73ee3f4ea
Thomas Drake <thomas.drake@live.fr>
2023-11-05 04:15:36 +0100

    forgot this

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from "@/node_modules/next/link"
 export function NavBar()
 {
     return (
-        <div className='border-b shadow-lg fixed w-screen bg-white py-4 px-20'>
+        <div className='border-b shadow-lg fixed w-screen bg-white py-4 px-5 sm:px-20'>
             <p className='text-xl'><Link href='/'>The English portal</Link></p>
         </div>
     )
