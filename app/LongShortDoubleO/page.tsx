6d5a48974ed4c781ed4714d34c1d2c0bc5fffe92
AzureAD\ThomasDrake <thomas.drake@upslide.net>
2023-11-13 13:54:57 +0100

    small corrections to lesson 1 text

--- app/LongShortDoubleO/page.tsx
+++ app/LongShortDoubleO/page.tsx
@@ -49,19 +49,19 @@ export default function LongShortDoubleO()
             <h1 className='text-2xl'>Foot vs Goose ([ʊ]/[u:])</h1>
             <h2 className='text-xl pb-2'>Lesson summary</h2>
             <div className='paragraph-block lesson-paragraph'>
-                <p>The two vowels covered in this lesson are often replaced by French speakers by a short [u] sound (the French ⟨ou⟩, as in <i>coucou</i>), which matches none of the correct pronunciations.</p>
+                <p>The two vowels covered in this lesson are often replaced by French speakers with a short [u] sound (the French ⟨ou⟩, as in <i>coucou</i>), which matches neither of the correct pronunciations.</p>
                 <p>Unfortunately you cannot always know when to use which, as English is not good at following rules when it comes to pronunciation.</p>
                 <p>Let&apos;s begin with presenting both sounds:</p>
                 <ul>
                     <li><p className='list-item-title'>The [ʊ] sound</p>
-                        <p className='list-item-detail'>It is <strong>short</strong>, and its realization depends on the speaker&apos;s accent.<br/>
+                        <p className='list-item-detail'>It is <strong>short</strong>, and its realisation depends on the speaker&apos;s accent.<br/>
                         Speakers of RP English use the authentic [ʊ] sound, which is somewhere in between a French [ø] (⟨eu⟩, as in <i>jeu</i>) and [o] (as in <i>beau</i>).<br />
                         Speakers of SSB English use a more relaxed sound, [ɵ]/[ʏ], which is very close to the French [ø] (<i>jeu</i>), but slightly less rounded (same as in <i>jeu</i> but lips do not form an &quot;o&quot;)
                         </p>
                     </li>
                     <li><p className='list-item-title'>The [u:] sound</p>
                         <p className='list-item-detail'>
-                        It is <strong>long</strong>, and its realization depends on the speaker&apos;s accent too.<br />
+                        It is <strong>long</strong>, and its realisation depends on the speaker&apos;s accent too.<br />
                         Speakers of RP English use the authentic [u:] sound, which is close to the French [y] (<i>rue</i>), but held longer.<br />
                         Speakers of SSB English use a sound often transcribed as [ᵾw], that could be approximated with an [øy] (<i>jeuw</i>).
                         </p>
@@ -310,7 +310,8 @@ export default function LongShortDoubleO()
             <h2 className='text-xl pb-2'>References</h2>
             <div className='paragraph-block'>
                 <ul>
-                    <li><a href='https://thesoundofenglish.org/oo-pronunciation-rules/'>https://thesoundofenglish.org/oo-pronunciation-rules/</a></li>
+                    <a href='https://thesoundofenglish.org/oo-pronunciation-rules/'><li><div className='hover:bg-neutral-200'>OO pronunciation rules (thesoundofenglish.org)</div></li></a>
+                    <a href='https://thesoundofenglish.org/ipa/'><li><div className='hover:bg-neutral-200'>IPA Chart (thesoundofenglish.org)</div></li></a>
                 </ul>
             </div>
         </main>
